59cbc48065831ed0f03389a92ef724ad317a0ef9
Yonny Rodriguez <jhrodrigu@hotmail.com>
2023-11-04 01:32:04 -0500

    creacion cabezote parte 1

---
+++ vistas/modulos/cabezote.php
@@ -0,0 +1,51 @@
+<!-- TOP -->
+
+
+<div class="container-fluid barraSuperior " id="top">
+  <div class="container">
+    <div class="row">
+      <!-- SOCIAL -->
+        <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+          <ul>
+            <li>
+              <a href="#" target="_blank">
+                <i class="fa fa-facebook redSocial facebookBlanco" aria-hidden="true"></i>
+            </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <i class="fa fa-youtube redSocial youtubeBlanco" aria-hidden="true"></i>
+            </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <i class="fa fa-twitter redSocial twitterBlanco" aria-hidden="true"></i>
+            </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <i class="fa fa-google-plus redSocial googleBlanco" aria-hidden="true"></i>
+            </a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                <i class="fa fa-instagram redSocial instagramBlanco" aria-hidden="true"></i>
+            </a>
+            </li>
+          </ul>
+        </div>
+        <!-- REGISTRO -->
+        <div class="col-lg-3 col-md-9 col-sm-8 colxs-12 registro">
+            <ul>
+              <li>
+                <a href="#modalIngreso" data-toggle="modal">Ingresar</a>
+              </li>
+              <li>
+                <a href="#modalRegistro" data-toggle="modal">Crear una cuenta</a>
+              </li>
+            </ul>
+        </div>
+
+    </div>
+  </div>
+</div>
